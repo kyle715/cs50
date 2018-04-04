0bb7bd2e934435084db28cb67a10c3f8fa86b23a
Kyle Mayers <kyle7150@gmail.com>
2018-04-04 10:28:26 +0000

    finish pset1

---
+++ pset1/cash.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+
+    int main(void)
+    {
+    float n;
+    int coins = 0;
+        do {
+            n = get_float("change due: ");
+        } while (n > 1 || n < 0);{
+            while( n >= .25){
+            n = ( n - .25);
+            coins++;
+            } while (n >= .10){
+                n = ( n - .10);
+                coins++;
+            } while ( n >= .05){
+                n = (n - .05);
+                coins++;
+            } while (n > .01 || n == .01){
+                n = (n - .01);
+                coins++;
+            } // while( n = .01){
+            //     n = (n - .01);
+            //     coins++;
+            // }
+        printf("%i", coins);
+        };
+
+    }
--- pset1/mario/less.c
+++ pset1/mario/less.c
@@ -7,7 +7,14 @@ int main(void)
     do {
         n = get_int("How big should the pryamid be?: ");
     } while (n > 23 || n < 0);
-    {
-        printf("%i", n);
+        {
+        for ( int i = 0; i < n; i++){
+            for (int spaces = n - i; spaces > 1; spaces--){
+                printf(" ");
+            } for ( int hash = 0; hash < i + 2; hash ++){
+                printf("#");
+            }
+            printf("\n");
+          };
     };
 }
